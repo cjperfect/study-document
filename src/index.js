1bc1be33f63fb66b3dec49888da62e39c2aca296
cjperfect <1982145923@qq.com>
2024-05-21 15:39:45 +0800

    log jsx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div>cxkyyds</div>);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<div>cxkyyds</div>);
+
+ReactDOM.render(
+  <div className="container">
+    <span>chenjiang</span>
+  </div>,
+  document.getElementById("root")
+);
+
+console.log(
+  <div className="container">
+    <span>chenjiang</span>
+  </div>
+);
